c90dd997dd3d85996dbfa7d234802fa8c95c123d
Mikhail Mishin <excentro@gmail.com>
2020-11-29 14:16:01 +0300

    добавил cloud gateway

---
+++ shop-gateway/src/main/java/com/excentro/ShopGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.excentro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopGatewayApplication.class, args);
+	}
+
+}
