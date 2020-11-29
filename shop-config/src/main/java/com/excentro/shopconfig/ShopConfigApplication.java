be53ac3e18e204c3c298b165fa164e976ef83379
Mikhail Mishin <excentro@gmail.com>
2020-11-29 13:01:15 +0300

    добавил cloud config server

---
+++ shop-config/src/main/java/com/excentro/shopconfig/ShopConfigApplication.java
@@ -0,0 +1,14 @@
+package com.excentro.shopconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ShopConfigApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShopConfigApplication.class, args);
+  }
+}
---
+++ shop-config/src/test/java/com/excentro/shopconfig/ShopConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excentro.shopconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
